701077d7656308b11acc6a4f9d47cfea7923c41b
jacek <jacek.miloszewski@protonmail.com>
2024-09-22 09:39:06 +0100

    finished home

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="min-h-dvh flex flex-col">
+  <div class="min-h-dvh flex flex-col min-w-80">
     <div class="h-14 bg-emerald-500">
       <div class="container mx-auto h-full flex flex-col justify-center">
         <div class="text-neutral-100 text-xl font-semibold mx-auto">
@@ -11,9 +11,12 @@
     <div class="container py-12 max-w-6xl mx-auto flex-grow">
       <router-view :key="$route.fullPath" />
     </div>
-    <div class="container mx-auto max-w-6xl mt-5">
-      <div class="mx-auto my-2 w-fit">
-        Contact: <a href="mailto:admin@hammergen.net" class="text-blue-500 hover:text-blue-900">admin@hammergen.net</a>
+    <div class="border-t border-neutral-300">
+      <div class="container mx-auto max-w-6xl">
+        <div class="mx-auto my-4 w-fit">
+          Contact:
+          <a href="mailto:admin@hammergen.net" class="text-blue-500 hover:text-blue-900">admin@hammergen.net</a>
+        </div>
       </div>
     </div>
   </div>
--- web/src/views/HomePage.vue
+++ web/src/views/HomePage.vue
@@ -1,64 +1,67 @@
 <template>
-  <div class="text-2xl text-center">Roll RPG dices with friends.</div>
-  <div class="field input-max-width">
-    <div class="mt-10 max-w-md mx-auto px-5">
+  <div class="max-w-3xl mx-auto px-5">
+    <div class="text-2xl text-center">Roll RPG dices with friends.</div>
+    <div class="mt-10 max-w-md mx-auto">
       <input
         v-model="username"
-        class="border border-neutral-300 rounded w-full h-10 px-2 focus:outline-emerald-700 focus:border-transparent focus:outline focus:outline-2 disabled:bg-neutral-200"
+        class="border border-neutral-300 rounded w-full h-10 px-2 focus:outline-emerald-600 focus:border-transparent focus:outline focus:outline-2"
         placeholder="Your Name"
       />
-    </div>
-  </div>
-  <div class="mt-2 max-w-md mx-auto px-5">
-    <div
-      class="flex items-stretch focus-within:outline-emerald-600 focus-within:outline-2 focus-within:outline rounded"
-    >
-      <div class="flex-grow">
-        <input
-          v-model="sessionId"
-          class="border-b border-l border-t border-neutral-300 rounded-l w-full h-10 px-2 focus:border-transparent outline-0"
-          placeholder="Session ID"
-        />
+      <div
+        class="mt-2 flex items-stretch focus-within:outline-emerald-600 focus-within:outline-2 focus-within:outline rounded"
+      >
+        <div class="flex-grow">
+          <input
+            v-model="sessionId"
+            class="border-b border-l border-t border-neutral-300 rounded-l w-full h-10 px-2 focus:border-transparent outline-0"
+            placeholder="Session ID"
+          />
+        </div>
+        <div class="bg-emerald-500 rounded-r hover:bg-emerald-600">
+          <button class="px-5 h-10 text-neutral-100 w-full" @click="joinSession">Join</button>
+        </div>
       </div>
-      <div class="bg-emerald-500 rounded-r hover:bg-emerald-600">
-        <button class="px-5 h-10 text-neutral-100 w-full" @click="joinSession">Join</button>
-      </div>
+      <p v-if="invalidSession" class="mt-1 text-red-600 text-xs text-center">
+        ID must be a combination of 6 letters and/or numbers.
+      </p>
     </div>
-    <p v-if="invalidSession" class="mt-1 text-red-600 text-xs text-center">
-      ID must be a combination of 6 letters and/or numbers.
-    </p>
-  </div>
 
-  <div class="mx-auto text-center my-1">or</div>
+    <div class="mx-auto text-center my-1">or</div>
 
-  <div class="mx-auto">
-    <button
-      class="ml-1 mr-1 mb-2"
-      :class="submitting ? 'button is-success is-loading' : 'button is-success'"
-      @click="createSession"
-    >
-      Create Session
-    </button>
-    <button :disabled="lastSessionDisabled" class="button is-success ml-1 mr-1" @click="joinLastSession">
-      Join Last Session
-    </button>
-  </div>
+    <div class="mx-auto flex justify-center gap-4">
+      <button
+        class="bg-emerald-500 rounded hover:bg-emerald-600 px-2 py-2 text-neutral-100 w-32"
+        :class="submitting ? 'pointer-events-none bg-emerald-600' : ''"
+        @click="createSession"
+      >
+        <AnimatedSpinner v-if="submitting" class="mx-auto" />
+        {{ submitting ? "" : "Create Session" }}
+      </button>
+      <button
+        :disabled="lastSessionDisabled"
+        class="bg-emerald-500 rounded hover:bg-emerald-600 px-2 py-2 text-neutral-100"
+        @click="joinLastSession"
+      >
+        Join Last Session
+      </button>
+    </div>
 
-  <div v-if="errors.length">
-    <div v-for="error in errors" :key="error" class="help is-danger">{{ error }}</div>
-  </div>
+    <div v-if="errors.length" class="mt-2">
+      <div v-for="error in errors" :key="error" class="text-red-600 text-center">{{ error }}</div>
+    </div>
 
-  <div class="is-size-4 mb-3 mt-5 has-text-left">What can I do here?</div>
-  <div class="has-text-left">
-    <p class="mb-2">
-      Hammergen Dice is an online multiplayer dice roller. You can roll RPG dices by yourself or with friends. To start
-      rolling simply hit "Create New Session" button.
-    </p>
-    <p class="mb-2">
-      If you want to roll with friends, create a new session and give them 6 character session ID number which they can
-      you to join the session. Alternatively, after creating a session, you can just copy and give them session URL, for
-      example: {{ domain }}/s/V4A6nA. Session expires after 8 hours.
-    </p>
+    <div class="text-2xl mb-4 mt-8">What can I do here?</div>
+    <div>
+      <p class="mb-2">
+        Hammergen Dice is an online multiplayer dice roller. You can roll RPG dices by yourself or with friends. To
+        start rolling simply hit "Create New Session" button.
+      </p>
+      <p class="mb-2">
+        If you want to roll with friends, create a new session and give them 6 character session ID number which they
+        can you to join the session. Alternatively, after creating a session, you can just copy and give them session
+        URL, for example: {{ domain }}/s/V4A6nA. Session expires after 8 hours.
+      </p>
+    </div>
   </div>
 </template>
 
@@ -67,6 +70,7 @@ import { ref, computed } from "vue";
 import axios from "axios";
 import randomName from "../randomName";
 import { useRouter } from "vue-router";
+import AnimatedSpinner from "../components/AnimatedSpinner.vue";
 
 const props = defineProps<{
   initialUsername?: string;
@@ -149,4 +153,4 @@ async function createSession() {
 }
 </script>
 
-<style></style>
+<style scoped></style>
