62836afe6f2f85a9c5038e164c9d28611565dcc8
jacek <jacek.miloszewski@protonmail.com>
2024-09-24 12:48:59 +0100

    moved env variables

--- web/src/firebase.ts
+++ web/src/firebase.ts
@@ -2,15 +2,17 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 import { getAnalytics } from "firebase/analytics";
 
+const projectId = import.meta.env.VITE_FIREBASE_PROJECT_ID;
+
 export const firebaseApp = initializeApp({
-  apiKey: "AIzaSyCeP45XubdwtjY1s_E8GyDuYtNqfBHc32s",
-  authDomain: "dice-roller-c518a.firebaseapp.com",
-  databaseURL: "https://dice-roller-c518a.firebaseio.com",
-  projectId: "dice-roller-c518a",
-  storageBucket: "dice-roller-c518a.appspot.com",
-  messagingSenderId: "156397029967",
-  appId: "1:156397029967:web:c78eda7d63664da4b7b83a",
-  measurementId: "G-2PCZVSC01Z",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: `${projectId}.firebaseapp.com`,
+  databaseURL: `https://${projectId}.firebaseio.com`,
+  projectId: projectId,
+  storageBucket: `${projectId}.appspot.com`,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_GOOGLE_ANALYTICS_ID,
 });
 
 const db = getDatabase(firebaseApp);
